ba50ee57a344937d9c2a61f8564ee9ec1e14a202
Pablo Arias <pariasm@gmail.com>
2017-06-16 20:17:19 +0200

    Detect file extension in scripts

--- 10_preprocess_noise.sh
+++ 10_preprocess_noise.sh
@@ -10,13 +10,28 @@ fi
 SEQUENCE=$1
 echo "Preprocessing sequence $SEQUENCE. Output stored in output_data/1_preprocessing/$SEQUENCE/"
 
+# determine extension
+if [ -f "input_data/${SEQUENCE}/001.tif" ]
+then
+	EXT="tif"
+elif [ -f "input_data/${SEQUENCE}/001.tiff" ]
+then
+	EXT="tiff"
+elif [ -f "input_data/${SEQUENCE}/001.png" ]
+then
+	EXT="png"
+else
+	echo "File ${SEQUENCE}/001.{tif,tiff,png} not found"
+	exit 1
+fi
+
 
 # for now, we just create sym links in the output folder
 OUTPUT_DIR="output_data/1_preprocessing/$SEQUENCE"
 mkdir -p $OUTPUT_DIR
 BASE_DIR=$(pwd)
 cd $OUTPUT_DIR
-for i in $(ls ${BASE_DIR}/input_data/${SEQUENCE}/*tif);
+for i in $(ls ${BASE_DIR}/input_data/${SEQUENCE}/*$EXT);
 do
 	ln -s $i
 done
--- 20_stabilize_video.sh
+++ 20_stabilize_video.sh
@@ -16,4 +16,21 @@ INPUT_DIR="output_data/1_preprocessing/$SEQUENCE"
 OUTPUT_DIR="output_data/2_stabilization/$SEQUENCE"
 mkdir -p $OUTPUT_DIR
 
-$STABI $INPUT_DIR/%03d.tif 1 300 -1 -1 -1 -o $OUTPUT_DIR/%03d.tif 
+# determine extension
+if [ -f "${INPUT_DIR}/001.tif" ]
+then
+	EXT="tif"
+elif [ -f "${INPUT_DIR}/001.tiff" ]
+then
+	EXT="tiff"
+elif [ -f "${INPUT_DIR}/001.png" ]
+then
+	EXT="png"
+else
+	echo "File ${INPUT_DIR}/001.{tif,tiff,png} not found"
+	exit 1
+fi
+
+NFRAMES=$(ls $INPUT_DIR/*$EXT | wc -l)
+
+$STABI $INPUT_DIR/%03d.$EXT 1 $NFRAMES -1 -1 -1 -o $OUTPUT_DIR/%03d.tif 
