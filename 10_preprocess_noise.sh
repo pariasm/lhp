1c5fd7bc50f276c1a9054bd893124002e9fc7b2f
parias <pariasm@gmail.com>
2017-06-16 14:29:27 +0200

    Placeholder for the noise preprocessing script

--- 10_preprocess_noise.sh
+++ 10_preprocess_noise.sh
@@ -12,3 +12,12 @@ echo "Preprocessing sequence $SEQUENCE. Output stored in output_data/1_preproces
 
 # do things e.g. call helper script
 ./11_remove_impulse_noise.sh $SEQUENCE
+
+
+# for now, we just create sym links in the output folder
+OUTPUT_DIR="output_data/1_preprocessing/$SEQUENCE"
+mkdir -p $OUTPUT_DIR
+for i in $(ls input_data/${SEQUENCE}/*tif);
+do
+	ln -s $i $OUTPUT_DIR
+done
