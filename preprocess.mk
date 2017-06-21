469b7fecb0365815b6620f3b53ce0d0093439c77
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2017-06-21 15:23:26 +0200

    complete preprocessing script

--- preprocess.mk
+++ preprocess.mk
@@ -1,37 +1,63 @@
 INPUTS   = $(shell ls i*tif)
 LINEAR   = $(addprefix lin_,$(INPUTS))
+UNBAND   = $(addprefix  ub_,$(LINEAR))
+MASKED   = $(addprefix   m_,$(UNBAND))
+FILLED   = $(addprefix   s_,$(MASKED))
 
-all: avglin.tif stdlin.tif
+all: $(FILLED)
 
 # linearization: contrast change to achieve linear scaling of gray values
 lin_i%.tif: i%.tif
 	plambda $^ vavg\ log -o $@
 
 # pointwise average of linearized images
-avglin.tif: $(LINEAR)
+avg_lin.tif: $(LINEAR)
 	veco -g finite avg $^ -o $@
 
 # pointwise standard deviation of linearized images
-stdlin.tif: $(LINEAR)
+std_lin.tif: $(LINEAR)
 	veco -g finite std $^ -o $@
 
-# robustified min and max
-range.txt: avglin.tif
-	imprintf "%q[1] %q[99]\n" $^ > $@
+# robustified min and max for conversion to and from png
+range.txt: avg_lin.tif
+	imprintf "%q[1] %q[99]\n" avg_lin.tif > range.txt
 
-# quantized avglin (using simplest color balance)
-avglin.png: avglin.tif range.txt
-	plambda avglin.tif "`cat range.txt` qe" -o avglin.png
+# quantized avg_lin (using simplest color balance)
+avg_lin.png: avg_lin.tif range.txt
+	plambda avg_lin.tif "`cat range.txt` qe" -o avg_lin.png
 
-# find bands using Yohann's program
-ub_avglin.png: avglin.png
-	demo_MIRE $^ $@
+# remove bands using Yohann's program
+ub_avg_lin.png: avg_lin.png
+	demo_MIRE $< $@
 
-# extract normalized bands
-#bands.tif: ub_avglin.png
-#	plambda avglin.png ub_avglin.png - | veco -c avg - | plambda av
+# extract image of bands
+bands.tif: avg_lin.png ub_avg_lin.png range.txt
+	plambda ub_avg_lin.png "`cat range.txt` iqe" -o ub_avg_lin.tif
+	plambda avg_lin.tif ub_avg_lin.tif - -o bands2d.tif
+	veco -c med bands2d.tif | plambda avg_lin.tif - "" -o bands.tif
 
+# remove bands from each frame
+ub_lin_i%.tif: lin_i%.tif bands.tif
+	plambda $^ - -o $@
 
+# pixelwise standard deviation
+std_ub_lin.tif: $(UNBAND)
+	veco -g finite std $^ -o $@
 
+# global mask of "bad" pixels
+global_mask.png: std_ub_lin.tif
+	plambda $< "x x%W5000 > x x%W995000 < and" -o $@
+
+# apply global and local masks
+m_ub_%.tif: ub_%.tif global_mask.png
+	plambda $^ "x x%W5000 > x x%W995000 < and y and x nan if" -o $@
+
+# inpaint remaining holes
+s_m_%.tif: m_%.tif
+	simpois -i $< -o $@ 2>/dev/null
+
+# cleanup
 clean:
-	$(RM) $(LINEAR) avglin.tif stdlin.tif ub_avglin.png avglin.png bands.tif
+	$(RM) $(LINEAR) avg_lin.tif std_lin.tif ub_avg_lin.png avg_lin.png \
+	range.txt bands.tif ub_avg_lin.tif ub_lin_*.tif bands2d.tif \
+	m_ub_*.tif s_m_ub_*.tif std_ub_lin.tif global_mask.png
