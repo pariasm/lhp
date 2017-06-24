5e60a1015eefeb05cd162703536713fb64b41100
Pablo Arias <pariasm@gmail.com>
2017-06-25 01:34:17 +0200

    [nlbayes] 2nd step baricenter usingn basic estimate to smooth results

--- src/4_denoising/nlbayes/src/NlBayes/VideoNLBayes.h
+++ src/4_denoising/nlbayes/src/NlBayes/VideoNLBayes.h
@@ -31,7 +31,7 @@
 /* Corrects the 'centering bug' discovered by Nicola. In the second step, basic
  * and noisy patches are centered using the basic baricenter. If left undefined,
  * each set of patches (noisy and basic) are centered using their own baricenter. */
-//#define BARICENTER_BASIC
+#define BARICENTER_BASIC
 
 /* Select a variance estimation method.
  *
