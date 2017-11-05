e6bfc194215d577d7d289213ed8a4834e4e7fa9b
Pablo Arias <pariasm@lime>
2017-11-05 23:29:16 +0100

    train script: psnr away from borders.

--- src/4_denoising/rbilf/scripts/vnlm_train.sh
+++ src/4_denoising/rbilf/scripts/vnlm_train.sh
@@ -24,25 +24,31 @@ FLOW="$SEQ/s${SIG}/tvl1_%03d_b.flo"
  -i $NISY -o $FLOW -f $FFR -l $LFR -s $SIG \
  -d $OUT"/deno_%03d.tif" $PRM
 
-# compute psnr
-SS=0
+# frame-by-frame psnr
 for i in $(seq $FFR $LFR);
 do
-	MM[$i]=$(psnr.sh $(printf $ORIG $i) $(printf $OUT/"deno_%03d.tif" $i) m)
-	n=$((i - FFR))
-	SS=$(echo "($SS*$n + ${MM[$i]})/$((n+1))" | bc -l)
+	# we remove a band of 10 pixels from each side of the frame
+	MM[$i]=$(psnr.sh $(printf $SEQ $i) $(printf $OUT/"deno_%03d.tif" $i) m 10)
 	MM[$i]=$(plambda -c "${MM[$i]} sqrt")
 	PP[$i]=$(plambda -c "255 ${MM[$i]} / log10 20 *")
 done
- 
-echo "Frame RMSE " ${MM[*]}  > $OUT/$(basename $SEQ).measures
-echo "Frame PSNR " ${PP[*]} >> $OUT/$(basename $SEQ).measures
- 
+
+echo "Frame RMSE " ${MM[*]}  > $OUT/measures
+echo "Frame PSNR " ${PP[*]} >> $OUT/measures
+
+# global psnr (from 4th frame)
+SS=0
+n=0
+for i in $(seq $((FFR+3)) $LFR);
+do
+	SS=$(plambda -c "${MM[$i]} 2 ^ $n $SS * + $((n+1)) /")
+	n=$((n+1))
+done
+
 RMSE=$(plambda -c "$SS sqrt")
 PSNR=$(plambda -c "255 $RMSE / log10 20 *")
-echo "Total RMSE $RMSE" >> $OUT/$(basename $SEQ).measures
-echo "Total PSNR $PSNR" >> $OUT/$(basename $SEQ).measures
-echo $PSNR
+echo "Total RMSE $RMSE" >> $OUT/measures
+echo "Total PSNR $PSNR" >> $OUT/measures
 
 
 # vim:set foldmethod=marker:
