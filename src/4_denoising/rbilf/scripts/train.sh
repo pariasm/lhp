bae590fa41ac2b2cc700611cdada2d7fe67a360a
Pablo Arias <pariasm@gmail.com>
2017-11-06 00:32:44 +0100

    Minor changes in train script

--- src/4_denoising/rbilf/scripts/train.sh
+++ src/4_denoising/rbilf/scripts/train.sh
@@ -9,7 +9,7 @@ pszs=(4 8 12)
 wszs=(5 10 15)
 
 # number of trials
-ntrials=100
+ntrials=1000
 
 # test sequences
 seqs=(\
@@ -18,45 +18,51 @@ derf/foreman_mono \
 derf/football_mono \
 derf/tennis_mono \
 derf/stefan_mono \
-tut/gsalesman \
 )
+#tut/gsalesman \
 
 # seq folder
-sf='/home/pariasm/Remote/avocat/denoising/data/'
+sf='/home/pariasm/denoising/data/'
+
+output=${1:-"trials"}
 
 for ((i=0; i < $ntrials; i++))
 do
 	# randomly draw noise level and parameters
 
+	# noise level
 	r=$(awk -v M=2 -v s=$RANDOM 'BEGIN{srand(s); print int(rand()*(M+1))}')
 	s=${sigmas[$r]}
 
+	# patch size
 #	r=$(awk -v M=2 -v s=$RANDOM 'BEGIN{srand(s); print int(rand()*(M+1))}')
 #	p=${pszs[$r]}
 
+	# search region
 #	r=$(awk -v M=2 -v s=$RANDOM 'BEGIN{srand(s); print int(rand()*(M+1))}')
 #	w=${wszs[$r]}
 
 	p=8
 	w=10
 
+	# spatial and temporal weights
 	W=$((4*s*s))
 	whx=$(awk -v M=$W -v s=$RANDOM 'BEGIN{srand(s); print int(rand()*(M+1))}')
 	wht=$(awk -v M=$W -v s=$RANDOM 'BEGIN{srand(s); print int(rand()*(M+1))}')
-#	whv=$(awk -v M=$W -v s=$RANDOM 'BEGIN{srand(s); print int(rand()*(M+1))}')
-	whv=0
+	whv=$(awk -v M=$W -v s=$RANDOM 'BEGIN{srand(s); print int(rand()*(M+1))}')
+#	whv=0
 
-	lambda=$(awk -v s=$RANDOM 'BEGIN{srand(s); print rand()}')
+	lambtv=$(awk -v s=$RANDOM 'BEGIN{srand(s); print rand()}')
 
-	trialfolder=$(printf "trials/rnlm.s%02d.p%02d.w%02d.whx%04d.wht%04d.whv%04d.lambda%5.3f\n" \
-		$s $p $w $whx $wht $whv $lambda)
+	trialfolder=$(printf "$output/rnlm.s%02d.p%02d.w%02d.whx%04d.wht%04d.whv%04d.lambtv%5.3f\n" \
+		$s $p $w $whx $wht $whv $lambtv)
 
-	params=$(printf " -p %d -w %d --whx %d --wht %d --whtv %d --lambda %f" \
-		$p $w $whx $wht $whv $lambda)
+	params=$(printf " -p %d -w %d --whx %d --wht %d --whtv %d --lambtv %f" \
+		$p $w $whx $wht $whv $lambtv)
 
 	mpsnr=0
 	nseqs=${#seqs[@]}
-	nf=10
+	nf=15
 	if [ ! -d $trialfolder ]
 	then
 		for seq in ${seqs[@]}
@@ -70,7 +76,7 @@ do
 	fi
 	
 	printf "%2d %2d %2d %4d %4d %4d %5.3f %7.4f\n" \
-		$s $p $w $whx $wht $whv $lambda $mpsnr >> trials/table
+		$s $p $w $whx $wht $whv $lambtv $mpsnr >> $output/table
 
 	rm $trialfolder/*.tif
 
