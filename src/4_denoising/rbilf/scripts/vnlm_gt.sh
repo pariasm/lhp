ff11c0ecf19c25a483291aca87e8baee7b2e9a48
parias <pariasm@gmail.com>
2017-11-05 22:36:38 +0100

    vnlm script: downscaling for oflow computation and psnr away from video borders

--- src/4_denoising/rbilf/scripts/vnlm_gt.sh
+++ src/4_denoising/rbilf/scripts/vnlm_gt.sh
@@ -11,6 +11,7 @@ PRM=$6 # denoiser parameters
 mkdir -p $OUT/s$SIG
 OUT=$OUT/s$SIG
 
+# error checking {{{1
 for i in $(seq $FFR $LFR);
 do
 	file=$(printf $SEQ $i)
@@ -47,24 +48,68 @@ do
 done
 cp $(printf $OUT"/%03d_b.flo" $((FFR+1))) $(printf $OUT"/%03d_b.flo" $FFR)
 
+# # downsample for optical flow {{{1
+# for i in $(seq $FFR $LFR);
+# do
+# 	filein=$(printf $OUT/"%03d.tif" $i)
+# 	file=$(printf $OUT/"h%03d.tif" $i)
+# 	if [ ! -f $file ]
+# 	then
+# 		blur gauss 1.39 $filein | downsa f 2 - $file
+# 	fi
+# done
+# 
+# # compute optical flow {{{1
+# TVL1="/home/pariasm/Work/optical_flow/algos/tvl1flow_3/tvl1flow"
+# for i in $(seq $((FFR+1)) $LFR);
+# do
+# 	file=$(printf $OUT"/h%03d_b.flo" $i)
+# 	if [ ! -f $file ]
+# 	then
+# 		$TVL1 $(printf $OUT"/h%03d.tif" $i) \
+# 				$(printf $OUT"/h%03d.tif" $((i-1))) \
+# 				$file \
+# 				0 0.25 0.2 0.3 100 0.5 5 0.01 0; 
+# 	fi
+# done
+# cp $(printf $OUT"/h%03d_b.flo" $((FFR+1))) $(printf $OUT"/h%03d_b.flo" $FFR)
+# 
+# # upscale optical flow {{{1
+# for i in $(seq $FFR $LFR);
+# do
+# 	filein=$(printf $OUT/"h%03d_b.flo" $i)
+# 	file=$(printf $OUT/"%03d_b.flo" $i)
+# 	if [ ! -f $file ]
+# 	then
+# 		plambda $filein "2 x *" | upsa 2 2 - $file
+# 	fi
+# done
+
 # run denoising {{{1
 ../build/bin/vnlmeans \
  -i $OUT"/%03d.tif" -o $OUT"/%03d_b.flo" -f $FFR -l $LFR -s $SIG \
  -d $OUT"/deno_%03d.tif" $PRM
 
 # compute psnr {{{1
-SS=0
 for i in $(seq $FFR $LFR);
 do
-	MM[$i]=$(psnr.sh $(printf $SEQ $i) $(printf $OUT/"deno_%03d.tif" $i) m)
-	n=$((i - FFR))
-	SS=$(echo "($SS*$n + ${MM[$i]})/$((n+1))" | bc -l)
+	# we remove a band of 10 pixels from each side of the frame
+	MM[$i]=$(psnr.sh $(printf $SEQ $i) $(printf $OUT/"deno_%03d.tif" $i) m 10)
 	MM[$i]=$(plambda -c "${MM[$i]} sqrt")
 	PP[$i]=$(plambda -c "255 ${MM[$i]} / log10 20 *")
 done
 
 echo "Frame RMSE " ${MM[*]}  > $OUT/measures
 echo "Frame PSNR " ${PP[*]} >> $OUT/measures
+
+# Global measures (from 4th frame)
+SS=0
+n=0
+for i in $(seq $((FFR+3)) $LFR);
+do
+	SS=$(plambda -c "${MM[$i]} 2 ^ $n $SS * + $((n+1)) /")
+	n=$((n+1))
+done
 
 RMSE=$(plambda -c "$SS sqrt")
 PSNR=$(plambda -c "255 $RMSE / log10 20 *")
